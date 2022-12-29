3d1707f3ad06867036559f47e3278861398e77fc
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-12-29 20:30:25 +0900

    recap

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+const logger = morgan("dev");
 const PORT = 5000;
 
-const logger = (req, res, next) => {
-    console.log(`Someone is going to: ${req.url}`);
-    next();
-}
-
-const handleHome = (req, res) => {
-    return res.send("I love middlewares");
+const home = (req, res) => {
+    console.log("I will respond.");
+    return res.send("hello");
 };
 
-app.get("/", logger, handleHome);
-
+const login = (req, res) => {
+    return res.send("login");
+};
+app.use(logger)
+app.get("/", home);
+app.get("/login", login);
 const handleListening = () => 
     console.log(`Server Listening on Port http://localhost:${PORT} 💡`);
 
