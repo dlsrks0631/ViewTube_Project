5d4605708dbb8307e36e5586eff60b5c55fe9c58
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-12-30 22:25:46 +0900

    recap

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { join } from "../controllers/userController";
-import { trending } from "../controllers/videoController";
+import { join, login } from "../controllers/userController";
+import { trending, search } from "../controllers/videoController";
 
 const globalRouter = express.Router(); // /login /join /search
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
+
 
 export default globalRouter;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,10 @@
 import express from "express"
-import {edit, remove} from "../controllers/userController";
+import { edit, remove, logout, see } from "../controllers/userController";
 const userRouter = express.Router();   // users/edit   users/delete
 
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get(":id", see);
 
 export default userRouter;
