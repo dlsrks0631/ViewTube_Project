254c257b18e774a0cdbd7e71fc899d040098dcab
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-01-02 20:35:48 +0900

    ~6.1

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import { watch, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();  // videos/watch 
 
 
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
