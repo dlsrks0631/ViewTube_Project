74bf04d78d01a70b640b1df21c04a6b2eb2c9872
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-01-02 11:29:08 +0900

    recap ~ 5.3

--- src/server.js
+++ src/server.js
@@ -9,9 +9,11 @@ const PORT = 5000;
 
 const app = express();
 const logger = morgan("dev");
+
+app.set("view engine", "pug"); 
+app.set("views", process.cwd() + "/src/views");
+
 app.use(logger)
-
-
 app.use("/", globalRouter);
 app.use("/videos", videoRouter); // 누군가 와서 /videos로 시작하는 url을 찾는다면 videoRouter로 들어감
 app.use("/users", userRouter);
