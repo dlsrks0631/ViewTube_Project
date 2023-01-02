0de9c24deaf3e81e5e229b73fb64b0ab481ef6ba
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-01-02 19:15:40 +0900

    ~5.10 recap

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,36 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
+
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+    ];
+    return res.render("home", { pageTitle: "Home", videos });
+
+};
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
 export const edit = (req, res) => res.render("edit");
 export const search = (req,res) => res.send("Search");
 export const upload = (req,res) => res.send("Upload Video");
