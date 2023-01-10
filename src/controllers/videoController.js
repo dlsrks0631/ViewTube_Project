dd18f3a0f4ffab504d3b47e3dbaa63a90c88d4a2
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-01-10 23:54:38 +0900

    ~6.12

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,44 +1,25 @@
-let videos = [
-    {
-        title: "First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 1,
-    },
-    {
-        title: "Second Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 2,
-    },
+import Video from "../models/Video"
 
-    {
-        title: "Third Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 3,
-    },
-];
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", videos });
+export const home = (req, res) => {
+    Video.find({},(error, videos) => {
+        console.log("errors", error);
+        console.log("videos", videos);
+    }); 
+    return res.render("home", {pageTitle: "Home"});
+
+};
+
 export const watch = (req, res) => {
     const { id } = req.params;
-    return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+    return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id -1];
-    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+    return res.render("edit", {pageTitle: `Editing`});
 }
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
-    videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`);
 };
 
@@ -48,14 +29,6 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
     const { title } = req.body;
-    const newVideo = {
-        title: title,
-        rating: 0,
-        comments: 0,
-        createdAt: "just now",
-        views: 1,
-        id: videos.length + 1,
-    };
     videos.push(newVideo);
     return res.redirect("/");
 };
