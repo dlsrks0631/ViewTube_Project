78870ac4a3fb1bcc59a6f963a57d28a5c1cfa376
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-01-13 22:10:48 +0900

    ~6.20 Edit video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,14 +5,25 @@ export const home = async (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  if(!video) {
+    return res.render("404", {pageTitle: "Video not found" });
+  }
+  return res.render("watch", {pageTitle: video.title, video});
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if(!video) {
+    return res.render("404", {pageTitle: "Video not found" });
+
+  }
+  return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,8 @@ import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/
 const videoRouter = express.Router();  // videos/watch 
 
 
-videoRouter.route("/:id(\\d+)").get(watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})").get(watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 
