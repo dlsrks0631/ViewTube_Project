d653627c6bc96e6bafa0063dde1477101e5fddc5
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-01-15 00:12:31 +0900

    ~7.1

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
@@ -14,7 +14,7 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter); 
 app.use("/users", userRouter);
 
