8b963b5941a2473f5d01af6ae4395431b41c53b6
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-01-17 00:18:12 +0900

    ~7.6

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -28,7 +28,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found" });
+    return res.status(400).render("404", { pageTitle: "Video not found" });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -53,7 +53,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
