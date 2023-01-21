6e5a87fefe67adfd73ba57645f87e9d0f591da9e
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-01-21 19:45:51 +0900

    ~7.7

--- src/server.js
+++ src/server.js
@@ -1,23 +1,29 @@
-
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-
 const app = express();
 const logger = morgan("dev");
 
-app.set("view engine", "pug"); 
+app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 app.use("/", rootRouter);
-app.use("/videos", videoRouter); 
+app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 export default app;
-
-
