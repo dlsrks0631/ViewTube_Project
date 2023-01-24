01eab2881bd485f77077fe3091411748608624b3
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-01-24 19:47:32 +0900

    ~7.11

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -22,13 +23,14 @@ app.use(
   })
 );
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   req.sessionStore.all((error, sessions) => {
     console.log(sessions);
     next();
   });
 });
 
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
