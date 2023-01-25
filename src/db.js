3d42442f25186c37ca9e752bcc48a9edf32428d8
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-01-25 15:54:28 +0900

    ~7.14

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetubedb");
+mongoose.connect(process.env.DB_URL);
 // mongoose.connect("mongodb://127.0.0.1:27017/wetubedb", {
 //   useNewUrlParser: true,
 //   useUnifiedTopoloy: true,
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -15,11 +16,17 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
+console.log(process.env.COOKIE_SECRET);
+
 app.use(
   session({
-    secret: "Hello",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 10000,
+    },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
