200cd54405ee756f2d5f5f4c2d01f2eae98f9b4a
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-02-16 20:15:40 +0900

    ffmpeg

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,5 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,13 +7,19 @@ let stream; // 실시간으로 재생되는 무언가
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({log: true}};
+  await ffmpeg.load();
+
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4"); //1032
+
   const a = document.createElement("a");
   a.href = videoFile;
   a.download = "MyRecording.webm";
   document.body.appendChild(a);
   a.click();
-}
+};
 
 const handleStop = () => {
   startBtn.innerText = "Download Recording";
@@ -27,7 +35,7 @@ const handleStart = () => {
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
     // 브라우저의 메모리 상에 파일을 저장해두고 브라우저가 그 파일에 접근할 수 있는 URL을 줌
-    videoFile = URL.createObjectURL(event.data); 
+    videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
