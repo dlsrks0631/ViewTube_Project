41d88c942df8ff4df869f6765e808e9ae503d77b
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2023-02-28 00:03:31 +0900

    comment

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,23 +1,46 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text, id) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.dataset.id = id;
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  const span2 = document.createElement("span");
+  span2.innerText = "❌";
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  newComment.appendChild(span2);
+  videoComments.prepend(newComment);
+};
+
 const handleSubmit = async (event) => {
-  event.preventDefault();  // 브라우저가 항상 하는 동작을 멈추도록 함
+  event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
   const videoId = videoContainer.dataset.id;
-  if (text === "") {
+  if (text === "") {  // 댓글창이 비었는지 확인
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
-  textarea.value = "";
+
+  if (response.status === 201) {
+    textarea.value = "";
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
+  }
 };
+
 if (form) {
   form.addEventListener("submit", handleSubmit);
 }
